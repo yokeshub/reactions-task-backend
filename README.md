40b559e2ab864e206745bb0d83c6b5d50c1caeaf
yokeshhub <1794694@tcs.com>
2022-02-18 14:20:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# json-db-data
+A repository for mock json object data for my-json-server
+
+## try it!
+https://my-json-server.typicode.com/artfuldev/json-db-data
+https://artful-iudex.herokuapp.com/
---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.extended.json'));
+const middlewares = jsonServer.defaults();
+ 
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
